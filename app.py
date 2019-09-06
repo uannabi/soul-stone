e993089a83768e28d3b95cd3657d70c0b9b9ae98
zahid <zahid@cookupsapp.com>
2019-09-06 18:28:09 +0600

    twetter api and stream tweet on console

--- app.py
+++ app.py
@@ -1,12 +1,82 @@
+import sys
+import tweepy
+import json
+
 from chalice import Chalice
+import boto3
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+from tweepy import API
+import twitter
 
 app = Chalice(app_name='soul-stone')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
 
+
+@app.route('/')
+class MyStreamListener(StreamListener):
+
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        print((f"{tweet.user.name}  : {tweet.text}"))
+
+    def on_error(self, status_code):
+        return {"status": status_code}
+
+
+auth = OAuthHandler(customerKey, customerSecret)
+auth.set_access_token(accessToken, accessSecret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+tweets_listener = MyStreamListener(api)
+stream = tweepy.Stream(api.auth, tweets_listener)
+stream.filter(track=["Python", "Flask", "Django"], languages=["en"])
+
+# def index():
+#     timeline = api.home_timeline()
+#     for tweet in timeline:
+#         print(f"{tweet.user.name}  : {tweet.text}")
+
+# return {'hello': 'world'}
+# api = API(auth, wait_on_rate_limit=True,
+#           wait_on_rate_limit_notify=True)
+
+
+# class Listener(StreamListener):
+#     def __init__(self, output_file=sys.stdout):
+#         super(Listener, self).__init__()
+#         self.output_file = output_file
+#
+#     def on_status(self, status):
+#         print(status.text, file=self.output_file)
+#
+#     def on_error(self, status_code):
+#         print(status_code)
+#         return False
+#
+#
+# output = open('stream_output.txt', 'w')
+# listener = Listener(output_file=output)
+#
+# stream = Stream(auth=api.auth, listener=listener)
+# try:
+#     print('Start streaming.')
+#     stream.sample(languages=['en'])
+# except KeyboardInterrupt:
+#     print("Stopped.")
+# finally:
+#     print('Done.')
+#     stream.disconnect()
+#     output.close()
+
+
+#
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
